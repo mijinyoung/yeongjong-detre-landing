d5ce8587dff713c99431e5591ce66c811a8ad2f6
mijinyoung <mijinyoung@gmail.com>
2026-07-29 18:25:27 +0900

    v12 조감도 초점 및 커뮤니티 이미지 배치 개선

--- components/CommunityHighlights.tsx
+++ components/CommunityHighlights.tsx
@@ -16,21 +16,24 @@ export default function CommunityHighlights() {
               {section.titleLines[1]}
             </h2>
           </div>
-          <p className="bodyCopy">{section.intro}</p>
+          <div className="communityV120Intro">
+            <p className="bodyCopy">{section.intro}</p>
+            <figure className="communityV120Visual">
+              <Image
+                src={section.image}
+                alt={section.imageAlt}
+                fill
+                sizes="(max-width: 760px) 36vw, 240px"
+                className="communityV120Image"
+              />
+              <figcaption className="srOnly">
+                영종 디에트르 라 메르 커뮤니티 시스템 공식 안내
+              </figcaption>
+            </figure>
+          </div>
         </div>
 
         <div className="communityV120Layout">
-          <figure className="communityV120Visual">
-            <Image
-              src={section.image}
-              alt={section.imageAlt}
-              fill
-              sizes="(max-width: 900px) 100vw, 50vw"
-              className="communityV120Image"
-            />
-            <figcaption>OFFICIAL SYSTEM &amp; COMMUNITY GUIDE</figcaption>
-          </figure>
-
           <div className="communityV120Grid">
             {section.items.map((item, index) => (
               <article className="communityV120Card" key={item.title}>
